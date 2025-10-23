1fdb51fd3844fc2502087dc66a42662ba38a3a59
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2025-10-22 21:37:13 -0500

    Update make_slackware_elem.sh

--- make_slackware_elem.sh
+++ make_slackware_elem.sh
@@ -2202,6 +2202,7 @@ SEARCH_CLOG_INPARENT=on
 PKGS_PRIORITY=( pantheon )
 REPOPLUS=( pantheon )
 MIRRORPLUS['pantheon']=https://reddoglinux.ddns.net/linux/pantheon/current-8/
+#MIRRORPLUS['pantheon']=https://slackware.uk/people/n4t3r/pantheon/current-8/
 EOPL
   cat <<EOPL > etc/slackpkg/greylist
 cairo
