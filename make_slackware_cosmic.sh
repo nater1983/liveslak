06cbd69def63fde5c4f03a3d9131b7230fe16c0b
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2025-11-16 20:06:09 -0600

    Update make_slackware_cosmic.sh

--- make_slackware_cosmic.sh
+++ make_slackware_cosmic.sh
@@ -2296,8 +2296,8 @@ WGETOPTS="--timeout=20 --tries=2"
 GREYLIST=off
 PKGS_PRIORITY=( cosmic )
 REPOPLUS=( cosmic )
-MIRRORPLUS['cosmic']=https://reddoglinux.ddns.net/linux/cosmic/x86_64/
-#MIRRORPLUS['cosmic']=https://slackware.uk/people/n4t3r/cosmic/x86_64/
+#MIRRORPLUS['cosmic']=https://reddoglinux.ddns.net/linux/cosmic/x86_64/
+MIRRORPLUS['cosmic']=https://slackware.uk/people/n4t3r/cosmic/x86_64/
 EOPL
 
 # add slackpkg+ blacklist ensuring we don't end up install-new-ing kde package sets
